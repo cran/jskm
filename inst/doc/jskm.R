03dea6ce0e5f7d5cdacc0787c21a6e2827d40221
Jinseob Kim <jinseob2kim@gmail.com>
2024-01-26 06:20:08 +0000

    version 0.5.3

--- inst/doc/jskm.R
+++ inst/doc/jskm.R
@@ -6,51 +6,65 @@ knitr::opts_chunk$set(
 library(jskm)
 
 ## -----------------------------------------------------------------------------
-#Load dataset
+# Load dataset
 library(survival)
 data(colon)
-fit <- survfit(Surv(time,status)~rx, data=colon)
+fit <- survfit(Surv(time, status) ~ rx, data = colon)
 
-#Plot the data
+# Plot the data
 jskm(fit)
-jskm(fit, table = T, pval = T, label.nrisk = "No. at risk", size.label.nrisk = 8, 
-     xlabs = "Time(Day)", ylabs = "Survival", ystratalabs = c("Obs", "Lev", "Lev + 5FU"), ystrataname = "rx",
-     marks = F, timeby = 365, xlims = c(0, 3000), ylims = c(0.25, 1), showpercent = T)
+jskm(fit,
+  table = T, pval = T, label.nrisk = "No. at risk", size.label.nrisk = 8,
+  xlabs = "Time(Day)", ylabs = "Survival", ystratalabs = c("Obs", "Lev", "Lev + 5FU"), ystrataname = "rx",
+  marks = F, timeby = 365, xlims = c(0, 3000), ylims = c(0.25, 1), showpercent = T
+)
 
 ## -----------------------------------------------------------------------------
-jskm(fit, ci = T, cumhaz = T,  mark = F, ylab = "Cumulative incidence (%)", surv.scale = "percent", pval =T, pval.size = 6, pval.coord = c(300, 0.7))
+jskm(fit, ci = T, cumhaz = T, mark = F, ylab = "Cumulative incidence (%)", surv.scale = "percent", pval = T, pval.size = 6, pval.coord = c(300, 0.7))
 
 ## -----------------------------------------------------------------------------
-jskm(fit, mark = F,  surv.scale = "percent", pval =T, table = T, cut.landmark = 500, showpercent = T)
+jskm(fit, mark = F, surv.scale = "percent", pval = T, table = T, cut.landmark = 500, showpercent = T)
 
 ## -----------------------------------------------------------------------------
 ## Make competing risk variable, Not real
 colon$status2 <- colon$status
 colon$status2[1:400] <- 2
 colon$status2 <- factor(colon$status2)
-fit2 <- survfit(Surv(time,status2)~rx, data=colon)
+fit2 <- survfit(Surv(time, status2) ~ rx, data = colon)
 jskm(fit2, mark = F, surv.scale = "percent", table = T, status.cmprsk = "1")
 jskm(fit2, mark = F, surv.scale = "percent", table = T, status.cmprsk = "1", showpercent = T, cut.landmark = 500)
 
 ## -----------------------------------------------------------------------------
+jskm(fit, theme = "jama", cumhaz = T, table = T, mark = F, ylab = "Cumulative incidence (%)", surv.scale = "percent", pval = T, pval.size = 6, pval.coord = c(300, 0.7))
+
+## -----------------------------------------------------------------------------
+jskm(fit, theme = "nejm", nejm.infigure.ratiow = 0.6, nejm.infigure.ratioh = 0.4, nejm.infigure.ylim = c(0, 0.7), cumhaz = T, table = T, mark = F, ylab = "Cumulative incidence (%)", surv.scale = "percent", pval = T, pval.size = 6, pval.coord = c(300, 0.7))
+
+## -----------------------------------------------------------------------------
 library(survey)
-data(pbc, package="survival")
-pbc$randomized <- with(pbc, !is.na(trt) & trt>0)
-biasmodel <- glm(randomized~age*edema,data=pbc)
+data(pbc, package = "survival")
+pbc$randomized <- with(pbc, !is.na(trt) & trt > 0)
+biasmodel <- glm(randomized ~ age * edema, data = pbc)
 pbc$randprob <- fitted(biasmodel)
 
-dpbc<-svydesign(id=~1, prob=~randprob, strata=~edema, data=subset(pbc,randomized))
+dpbc <- svydesign(id = ~1, prob = ~randprob, strata = ~edema, data = subset(pbc, randomized))
 
-s1 <-svykm(Surv(time,status>0) ~ 1, design = dpbc)
-s2 <-svykm(Surv(time,status>0) ~ sex, design = dpbc)
+s1 <- svykm(Surv(time, status > 0) ~ 1, design = dpbc)
+s2 <- svykm(Surv(time, status > 0) ~ sex, design = dpbc)
 
 svyjskm(s1)
 svyjskm(s2)
-svyjskm(s2, cumhaz = T, ylab = "Cumulative incidence(%)", surv.scale = "percent", showpercent = T) 
+svyjskm(s2, cumhaz = T, ylab = "Cumulative incidence(%)", surv.scale = "percent", showpercent = T)
 
 ## -----------------------------------------------------------------------------
-s3 <- svykm(Surv(time,status>0) ~ sex, design=dpbc, se = T)
+s3 <- svykm(Surv(time, status > 0) ~ sex, design = dpbc, se = T)
 svyjskm(s3)
 svyjskm(s3, ci = F, showpercent = T)
-svyjskm(s3, ci = F,  surv.scale = "percent", pval = T, table = T, cut.landmark = 1000)
+svyjskm(s3, ci = F, surv.scale = "percent", pval = T, table = T, cut.landmark = 1000)
+
+## -----------------------------------------------------------------------------
+svyjskm(s2, theme = "jama", pval = T, table = T, design = dpbc)
+
+## -----------------------------------------------------------------------------
+svyjskm(s2, theme = "nejm", nejm.infigure.ratiow = 0.4, nejm.infigure.ratioh = 0.4, nejm.infigure.ylim = c(0.2, 1), pval = T, table = T, design = dpbc)
 
